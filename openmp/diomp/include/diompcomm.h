958a218872311034d0eaad8153991656215f9d9d
Baodi Shan <bshan@login2.cm.cluster>
2023-05-10 18:39:40 -0400

    init of DiOMP

---
+++ openmp/diomp/include/diompcomm.h
@@ -0,0 +1,19 @@
+/*
+ * diompcomm.h
+ */
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DIOMP_COMM_H
+#define DIOMP_COMM_H
+
+namespace diomp{
+
+
+}
---
+++ openmp/diomp/readme.md
@@ -0,0 +1,26 @@
+<!--
+  Copyright 2023 Stony Brook University, TotalEnergies E&P USA 
+-->
+# Model of DiOMP
+
+## General
+
+- Rank
+  - Number of ranks
+  - Rank of myself
+
+
+## Memory Model
+- Base Functions
+  - Alloc
+  - Free
+
+- Private variable
+
+- Shared variable
+  - Put
+  - Get
+  - Owner
+  - Location
+
+## Active Message
