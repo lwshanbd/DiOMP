59f1efe3960c1b58fad2a4f854391d8f06ac892a
Baodi Shan <lwshanbd@gmail.com>
2023-11-27 17:22:39 -0500

    Update

---
+++ openmp/diomp/include/tools.h
@@ -0,0 +1,18 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#define GASNET_Safe(fncall) do {                                     \
+    int _retval;                                                     \
+    if ((_retval = fncall) != GASNET_OK) {                           \
+      fprintf(stderr, "ERROR calling: %s\n"                          \
+                   " at: %s:%i\n"                                    \
+                   " error: %s (%s)\n",                              \
+              #fncall, __FILE__, __LINE__,                           \
+              gasnet_ErrorName(_retval), gasnet_ErrorDesc(_retval)); \
+      fflush(stderr);                                                \
+      gasnet_exit(_retval);                                          \
+    }                                                                \
+  } while(0)
+
+
+#endif
---
+++ openmp/diomp/src/diompmem.cpp
@@ -0,0 +1,19 @@
+#include "diompmem.h"
+
+namespace diomp{
+MemoryManager::MemoryManager(){
+    NodesNum = gasnet_nodes();
+    NodeID = gasnet_mynode();
+    SegInfo = new gasnet_seginfo_t[NodesNum];
+    GASNET_Safe(gasnet_getSegmentInfo(SegInfo, NodesNum));
+}
+
+uintptr_t MemoryManager::getSegmentSpace(int node){
+    return SegInfo[node].size;
+}
+
+void* MemoryManager::getSegmentAddr(int node){
+    return SegInfo[node].addr;
+}
+
+} // namespace diomp
