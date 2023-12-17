6b4ebaa58e62956b0694011bf06cd35004bb0863
Baodi Shan <lwshanbd@gmail.com>
2023-12-17 04:53:53 -0500

    Update to GASNet-EX and coll

--- openmp/diomp/include/diomp.h
+++ openmp/diomp/include/diomp.h
@@ -16,6 +16,7 @@
 #include <cstdio>
 
 #include <gasnet.h>
+#include <gasnet_coll.h>
 #include <gasnet_tools.h>
 #include <gasnetex.h>
 
@@ -28,19 +29,58 @@ int omp_get_rank_num();
 
 #define PAGESZ GASNETT_PAGESIZE
 
-void omp_get(void *dest, gasnet_node_t node, void *src, size_t nbytes);
-void omp_put(gasnet_node_t node, void *dest, void *src, size_t nbytes);
+enum class omp_op : gex_OP_t {
+
+  // accessors
+  load = GEX_OP_GET,
+  store = GEX_OP_SET,
+  compare_exchange = GEX_OP_FCAS,
+
+  // arithmetic
+  add = GEX_OP_ADD,
+  fetch_add = GEX_OP_FADD,
+  sub = GEX_OP_SUB,
+  fetch_sub = GEX_OP_FSUB,
+  inc = GEX_OP_INC,
+  fetch_inc = GEX_OP_FINC,
+  dec = GEX_OP_DEC,
+  fetch_dec = GEX_OP_FDEC,
+  mul = GEX_OP_MULT,
+  fetch_mul = GEX_OP_FMULT,
+  min = GEX_OP_MIN,
+  fetch_min = GEX_OP_FMIN,
+  max = GEX_OP_MAX,
+  fetch_max = GEX_OP_FMAX,
+
+  // bitwise operations
+  bit_and = GEX_OP_AND,
+  fetch_bit_and = GEX_OP_FAND,
+  bit_or = GEX_OP_OR,
+  fetch_bit_or = GEX_OP_FOR,
+  bit_xor = GEX_OP_XOR,
+  fetch_bit_xor = GEX_OP_FXOR,
+};
+
+static gex_TM_t diompTeam;
+gex_Client_t diompClient;
+gex_EP_t diompEp;
+gex_Segment_t diompSeg;
+
+void omp_get(void *dst, gasnet_node_t node, void *src, size_t nbytes);
+void omp_put(gasnet_node_t node, void *dst, void *src, size_t nbytes);
+
 void *omp_get_space(int node);
 uintptr_t omp_get_length_space(int node);
 
-void diomp_barrier() {
-  gasnet_barrier_notify(0, GASNET_BARRIERFLAG_ANONYMOUS);
-  gasnet_barrier_wait(0, GASNET_BARRIERFLAG_ANONYMOUS);
-}
+void diomp_barrier();
 
-// void gasnet_get ( void *dest , gasnet_node_t node , void *src , size t nbytes
-// ) void gasnet_put ( gasnet node t node , void *dest , void *src , size t
-// nbytes )
+/* Collective Operation */
+// Broadcast
+void omp_bcast(void *data, size_t nbytes, gasnet_node_t node);
+
+void omp_allreduce(void *src, void *dst, size_t count, omp_op op);
+
+// Reduction to one
 
 /* ------------------------------------------------------------------------------------
  */
--- openmp/diomp/src/diomp.cpp
+++ openmp/diomp/src/diomp.cpp
@@ -22,18 +22,16 @@ void my_gasnet_handler(gasnet_token_t token, void *buf, size_t nbytes) {
 
 void diomp_init(int argc, char *argv[]) {
 
+  gex_Client_Init(&diompClient, &diompEp, &diompTeam, "diomp", &argc, &argv, 0);
   // Handler init
 
   gasnet_handlerentry_t handlers[1];
   handlers[0].index = 128;
   handlers[0].fnptr = (void (*)())my_gasnet_handler;
   size_t page_size = GASNET_PAGESIZE;
-  size_t segsize = 1024 * 1024 * 1024;
+  size_t segsize = 10000000000;
 
-  gasnet_init(NULL, NULL);
-  GASNET_Safe(gasnet_attach(
-      handlers, sizeof(handlers) / sizeof(gasnet_handlerentry_t), segsize, 0));
-
+  GASNET_Safe(gex_Segment_Attach(&diompSeg, diompTeam, segsize));
   MemManger = new diomp::MemoryManager();
 }
 
@@ -42,15 +40,27 @@ int omp_get_num_ranks() { return gasnet_nodes(); }
 int omp_get_rank_num() { return gasnet_mynode(); }
 
 void omp_get(void *dest, gasnet_node_t node, void *src, size_t nbytes) {
-  gasnet_get_bulk(dest, node, src, nbytes);
+  gex_RMA_GetNBI(diompTeam, dest, node, src, nbytes, 0);
 }
 
 void omp_put(gasnet_node_t node, void *dest, void *src, size_t nbytes) {
-  gasnet_put_bulk(node, dest, src, nbytes);
+  gex_RMA_PutNBI(diompTeam, node, dest, src, nbytes, GEX_EVENT_DEFER, 0);
 }
+
+void diomp_barrier() { gex_NBI_Wait(GEX_EC_ALL, 0); }
 
 void *omp_get_space(int node) { return MemManger->getSegmentAddr(node); }
 
 uintptr_t omp_get_length_space(int node) {
   return MemManger->getSegmentSpace(node);
+}
+
+void omp_bcast(void *data, size_t nbytes, gasnet_node_t node) {
+  gex_Event_Wait(gex_Coll_BroadcastNB(diompTeam, node, data, data, nbytes, 0));
+}
+
+void omp_allreduce(void *src, void *dst, size_t count, omp_op op) {
+  gex_Event_Wait(gex_Coll_ReduceToAllNB(diompTeam, dst, src, 
+                        GEX_DT_I32, sizeof(GEX_DT_I32), count,
+                        (gex_OP_t)op, NULL, NULL, 0));
 }
