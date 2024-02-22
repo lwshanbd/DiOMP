ffdddd2e8fec261c936a9560344881b720849342
Baodi Shan <lwshanbd@gmail.com>
2024-02-21 21:11:23 -0500

    Update barrier

--- openmp/diomp/src/diompmem.cpp
+++ openmp/diomp/src/diompmem.cpp
@@ -3,11 +3,12 @@
 namespace diomp{
 
 
-MemoryManager::MemoryManager(){
-    NodesNum = gasnet_nodes();
-    NodeID = gasnet_mynode();
+MemoryManager::MemoryManager(gex_TM_t gexTeam){
+    NodesNum = gex_TM_QuerySize(gexTeam);
+    NodeID = gex_TM_QueryRank(gexTeam);
     SegInfo = new gasnet_seginfo_t[NodesNum];
     GASNET_Safe(gasnet_getSegmentInfo(SegInfo, NodesNum));
+    //GASNET_Safe(gasnet_getSegmentInfo(SegInfo, NodesNum));
 }
 
 uintptr_t MemoryManager::getSegmentSpace(int node){
