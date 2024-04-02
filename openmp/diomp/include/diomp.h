36c8ec781937346134c13d0283424022c5b8773e
Baodi Shan <lwshanbd@gmail.com>
2024-04-02 16:57:40 -0400

    Update alloc and bug fix

--- openmp/diomp/include/diomp.h
+++ openmp/diomp/include/diomp.h
@@ -13,58 +13,59 @@
 #ifndef DIOMP_H
 #define DIOMP_H
 #define GASNET_PAR
-#include <cstdio>
+
 
 #include <gasnet.h>
 #include <gasnet_coll.h>
 #include <gasnet_tools.h>
 #include <gasnetex.h>
 
-extern "C" void __init_diomp();
 
-int omp_get_num_ranks();
-int omp_get_rank_num();
 
-#define EVERYTHING_SEG_HANDLERS()
-
-#define PAGESZ GASNETT_PAGESIZE
-
-enum class omp_op : gex_OP_t {
-
+typedef enum omp_op {
   // accessors
-  load = GEX_OP_GET,
-  store = GEX_OP_SET,
-  compare_exchange = GEX_OP_FCAS,
+  omp_op_load = GEX_OP_GET,
+  omp_op_store = GEX_OP_SET,
+  omp_op_compare_exchange = GEX_OP_FCAS,
 
   // arithmetic
-  add = GEX_OP_ADD,
-  fetch_add = GEX_OP_FADD,
-  sub = GEX_OP_SUB,
-  fetch_sub = GEX_OP_FSUB,
-  inc = GEX_OP_INC,
-  fetch_inc = GEX_OP_FINC,
-  dec = GEX_OP_DEC,
-  fetch_dec = GEX_OP_FDEC,
-  mul = GEX_OP_MULT,
-  fetch_mul = GEX_OP_FMULT,
-  min = GEX_OP_MIN,
-  fetch_min = GEX_OP_FMIN,
-  max = GEX_OP_MAX,
-  fetch_max = GEX_OP_FMAX,
+  omp_op_add = GEX_OP_ADD,
+  omp_op_fetch_add = GEX_OP_FADD,
+  omp_op_sub = GEX_OP_SUB,
+  omp_op_fetch_sub = GEX_OP_FSUB,
+  omp_op_inc = GEX_OP_INC,
+  omp_op_fetch_inc = GEX_OP_FINC,
+  omp_op_dec = GEX_OP_DEC,
+  omp_op_fetch_dec = GEX_OP_FDEC,
+  omp_op_mul = GEX_OP_MULT,
+  omp_op_fetch_mul = GEX_OP_FMULT,
+  omp_op_min = GEX_OP_MIN,
+  omp_op_fetch_min = GEX_OP_FMIN,
+  omp_op_max = GEX_OP_MAX,
+  omp_op_fetch_max = GEX_OP_FMAX,
 
   // bitwise operations
-  bit_and = GEX_OP_AND,
-  fetch_bit_and = GEX_OP_FAND,
-  bit_or = GEX_OP_OR,
-  fetch_bit_or = GEX_OP_FOR,
-  bit_xor = GEX_OP_XOR,
-  fetch_bit_xor = GEX_OP_FXOR,
-};
+  omp_op_bit_and = GEX_OP_AND,
+  omp_op_fetch_bit_and = GEX_OP_FAND,
+  omp_op_bit_or = GEX_OP_OR,
+  omp_op_fetch_bit_or = GEX_OP_FOR,
+  omp_op_bit_xor = GEX_OP_XOR,
+  omp_op_fetch_bit_xor = GEX_OP_FXOR,
+} omp_op_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern gex_TM_t diompTeam;
 extern gex_Client_t diompClient;
 extern gex_EP_t diompEp;
 extern gex_Segment_t diompSeg;
+
+void __init_diomp();
+
+int omp_get_num_ranks();
+int omp_get_rank_num();
 
 void omp_get(void *dst, gasnet_node_t node, void *src, size_t nbytes);
 void omp_put(gasnet_node_t node, void *dst, void *src, size_t nbytes);
@@ -73,24 +74,18 @@ void *omp_get_space(int node);
 uintptr_t omp_get_length_space(int node);
 
 void diomp_barrier();
+void diomp_waitRMA();
 
-/* Collective Operation */
-// Broadcast
+
 void omp_bcast(void *data, size_t nbytes, gasnet_node_t node);
 
-void omp_allreduce(void *src, void *dst, size_t count, omp_op op);
+// Experimental. Only for benchmark
+void omp_allreduce(void *src, void *dst, size_t count, int op);
 
-// Reduction to one
+void* omp_ralloc(size_t Size);
 
-/* ------------------------------------------------------------------------------------
- */
-/* segment management */
-#ifndef TEST_SEGSZ_PADFACTOR
-#define TEST_SEGSZ_PADFACTOR 1
+#ifdef __cplusplus
+}
 #endif
-
-#define TEST_SEGSZ_REQUEST (TEST_SEGSZ_PADFACTOR * TEST_SEGSZ)
-
-#define TEST_MINHEAPOFFSET alignup(128 * 4096, PAGESZ)
 
 #endif
--- openmp/diomp/src/diompmem.cpp
+++ openmp/diomp/src/diompmem.cpp
@@ -1,22 +1,86 @@
 #include "diompmem.h"
 
-namespace diomp{
+namespace diomp {
 
-
-MemoryManager::MemoryManager(gex_TM_t gexTeam){
-    NodesNum = gex_TM_QuerySize(gexTeam);
-    NodeID = gex_TM_QueryRank(gexTeam);
-    SegInfo = new gasnet_seginfo_t[NodesNum];
-    GASNET_Safe(gasnet_getSegmentInfo(SegInfo, NodesNum));
-    //GASNET_Safe(gasnet_getSegmentInfo(SegInfo, NodesNum));
+MemoryManager::MemoryManager(gex_TM_t gexTeam) {
+  NodesNum = gex_TM_QuerySize(gexTeam);
+  NodeID = gex_TM_QueryRank(gexTeam);
+  SegInfo = new gex_Seginfo_t[NodesNum];
+  for (int r = 0; r < NodesNum; r++) {
+    void *segment_base = 0;
+    size_t segment_size = 0;
+    gex_Event_Wait(gex_EP_QueryBoundSegmentNB(diompTeam, NodeID, &segment_base,
+                                              nullptr, &segment_size, 0));
+    SegInfo[r].SegStart = segment_base;
+    SegInfo[r].SegSize = segment_size;
+    SegInfo[r].SegRemain = segment_base;
+  }
+  LocalSegStart = SegInfo[NodeID].SegStart;
+  LocalSegRemain = SegInfo[NodeID].SegRemain;
+  LocalSegSize = SegInfo[NodeID].SegSize;
 }
 
-uintptr_t MemoryManager::getSegmentSpace(int node){
-    return SegInfo[node].size;
+uintptr_t MemoryManager::getSegmentSpace(int Rank) {
+  return SegInfo[Rank].SegSize;
 }
 
-void* MemoryManager::getSegmentAddr(int node){
-    return SegInfo[node].addr;
+void *MemoryManager::getSegmentAddr(int Rank) { return SegInfo[Rank].SegStart; }
+
+void *MemoryManager::allocate(size_t Size) {
+  if (size > getAvailableSize()) {
+    return nullptr;
+  }
+  void *Ptr = SegRemain;
+  SegRemain = (char *)SegRemain + Size;
+  MemBlocks.push_back({ptr, Size});
+  return ptr;
 }
+
+size_t MemoryManager::getAvailableSize() const {
+  return (char *)LocalSegStart + LocalSegSize - (char *)LocalSegStart;
+}
+
+size_t MemoryManager::getOffset(void *Ptr) {
+  size_t Offset = (char *)Ptr - (char *)LocalSegStart;
+  if (Offset < 0 || Offset > LocalSegSize)
+    return -1;
+  return Offset;
+}
+
+size_t MemoryManager::getOffset(void *Ptr, int Rank) {
+  size_t Offset = (char *)Ptr - (char *)SegInfo[Rank].SegStart;
+  if (Offset < 0 || Offset > SegInfo[Rank].SegSize)
+    return -1;
+  return Offset;
+}
+
+bool MemoryManager::validGlobalAddr(void *Ptr, int Rank){
+  if(Ptr == nullptr){
+    return false;
+  }
+
+  size_t Offset = (char *)Ptr - (char *)SegInfo[Rank].SegStart;
+  if (Ptr < 0 || Offset > SegInfo[Rank].SegSize)
+    return false;
+  return true;
+}
+
+void *MemoryManager::syncGlobalfromLocalAddr(void *Ptr, int Rank){
+  size_t offset = getOffset(Ptr);
+  void *GlobalPtr = SegInfo[Rank].SegStart;
+  if(!validGlobalAddr(GlobalPtr)){
+    return nullptr;
+  }
+  return GlobalPtr;
+}
+
+
 
 } // namespace diomp
+
+// template <typename T> T *shmem_alloc(size_t count) {
+//   size_t size = count * sizeof(T);
+//   void *ptr = memory_manager.allocate(size);
+//   static_assert(ptr == nullptr, "Allocat global memory failed\n");
+//   return static_cast<T *>(ptr);
+// }
