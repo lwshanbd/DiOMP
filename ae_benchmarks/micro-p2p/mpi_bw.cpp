0cc4832b7dabf0884a84877fdf7b3f2788e25454
Baodi Shan <lwshanbd@gmail.com>
2025-09-03 15:57:54 -0700

    name fix

--- ae_benchmarks/micro-p2p/mpi_bw.cpp
+++ ae_benchmarks/micro-p2p/mpi_bw.cpp
@@ -8,7 +8,8 @@
 
 #define WARMUP_ITERS 5
 #define TEST_ITERS 20
-#define MAX_MSG_SIZE 1L * 1024 * 1024 * 1024  // 2GB
+#define MIN_SIZE_BYTES 4  // 4 Bytes
+#define MAX_SIZE_BYTES (128L * 1024 * 1024)  // 128 MB
 #define WARMUP_RUNS 5
 
 double get_time_in_microseconds() {
@@ -25,11 +26,10 @@ double calculate_average_bandwidth(std::vector<double>& times, size_t size) {
     
     double avg_time_us = sum / (times.size() - 4);  // Average time in microseconds
     double avg_time_s = avg_time_us * 1e-6;         // Convert to seconds
-    printf("latency %.5f us, ", avg_time_us);
     return (size / (1024.0 * 1024.0)) / avg_time_s;  // MB/s
 }
 
-void run_benchmark(size_t msg_size) {
+void run_benchmark(size_t msg_size, const char* operation) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -74,7 +74,11 @@ void run_benchmark(size_t msg_size) {
             double start_time = get_time_in_microseconds();
             
             MPI_Win_fence(0, win);
-            MPI_Put(gpu_buf, msg_size, MPI_BYTE, 0, 0, msg_size, MPI_BYTE, win);
+            if (strcmp(operation, "put") == 0) {
+                MPI_Put(gpu_buf, msg_size, MPI_BYTE, 0, 0, msg_size, MPI_BYTE, win);
+            } else if (strcmp(operation, "get") == 0) {
+                MPI_Get(gpu_buf, msg_size, MPI_BYTE, 0, 0, msg_size, MPI_BYTE, win);
+            }
             MPI_Win_fence(0, win);
             
             double end_time = get_time_in_microseconds();
@@ -82,7 +86,14 @@ void run_benchmark(size_t msg_size) {
         }
         
         double avg_bandwidth = calculate_average_bandwidth(times, msg_size);
-        printf("%zu,%f,Valid\n", msg_size, avg_bandwidth);
+        double avg_time_us = 0;
+        for (size_t i = 2; i < times.size() - 2; i++) {
+            avg_time_us += times[i];
+        }
+        avg_time_us /= (times.size() - 4);
+        
+        printf("| %12zu | %12.3f | %12.3f | %8s | %6s |\n", 
+               msg_size, avg_bandwidth, avg_time_us, "Valid", operation);
     } else {
         for (int i = 0; i < TEST_ITERS; i++) {
             MPI_Win_fence(0, win);
@@ -101,16 +112,49 @@ int main(int argc, char **argv) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (rank == 1) {
-        printf("Size(bytes),Avg Bandwidth(MB/s),Data Validity\n");
+    // Parse command line arguments
+    const char* operation = "put";  // default to put for MPI
+    if (argc > 1) {
+        if (strcmp(argv[1], "put") == 0 || strcmp(argv[1], "get") == 0) {
+            operation = argv[1];
+        } else {
+            if (rank == 0) {
+                printf("Usage: %s [put|get]\n", argv[0]);
+                printf("Default operation is 'put'\n");
+            }
+            MPI_Finalize();
+            return 1;
+        }
+    }
+
+    if (rank == 0) {
+        char op_title[8];
+        strcpy(op_title, operation);
+        if (op_title[0] >= 'a' && op_title[0] <= 'z') {
+            op_title[0] = op_title[0] - 'a' + 'A';
+        }
+        
+        printf("\n");
+        printf("=== MPI %s Bandwidth Benchmark Results ===\n", op_title);
+        printf("+---------------+--------------+--------------+----------+--------+\n");
+        printf("| Size (bytes)  | Bandwidth    | Latency      | Status   | Op     |\n");
+        printf("|               |   (MB/s)     |    (us)      |          |        |\n");
+        printf("+---------------+--------------+--------------+----------+--------+\n");
     }
 
     // Run benchmark with different message sizes
-    for (size_t size = 1; size <= MAX_MSG_SIZE; size *= 2) {
-        run_benchmark(size);
+    for (size_t size = MIN_SIZE_BYTES; size <= MAX_SIZE_BYTES; size *= 2) {
+        run_benchmark(size, operation);
         MPI_Barrier(MPI_COMM_WORLD);
+    }
+    
+    if (rank == 0) {
+        printf("+---------------+--------------+--------------+----------+--------+\n");
+        printf("\n");
     }
 
     MPI_Finalize();
     return 0;
 }
+
+// clang++ mpi_bw.cpp -lcuda -fopenmp  --offload-arch=sm_80 -lnccl -L /global/cfs/cdirs/xpress/baodi/software/llvm-gpu/lib -L /global/cfs/cdirs/xpress/baodi/software/gasnet-gpu/lib -I /global/cfs/cdirs/xpress/baodi/software/gasnet-gpu/include/ofi-conduit/ -lgasnet-ofi-par -no-pie -lmpi -lhwloc -lrt -pthread -I /opt/cray/pe/mpich/8.1.30/ofi/gnu/12.3/include -L /opt/cray/pe/mpich/8.1.30/ofi/gnu/12.3/lib -L/opt/cray/libfabric/1.22.0/lib64 -lfabric -L /opt/cray/pe/pmi/6.1.13/lib -lpmi -lhugetlbfs -DDIOMP_ENABLE_CUDA -I $NCCL_HOME/include -L $NCCL_HOME/lib -o mpi_bw_get
