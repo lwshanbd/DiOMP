ea45ef1cdcb22ac5b915b6748f1a6aa4471fa093
Baodi Shan <lwshanbd@gmail.com>
2025-09-03 21:58:21 -0700

    Update Scripts

--- ae_benchmarks/micro-p2p/test_perlmutter.sh
+++ ae_benchmarks/micro-p2p/test_perlmutter.sh
@@ -1,12 +1,15 @@
 # Please make sure you have allocated 2 Nodes and each node has at least 1 GPU.
 chmod +x run.sh
+
+make all
 srun -n 2 ./diomp_bw get > diomp_bw_get.txt
 srun -n 2 ./diomp_bw put > diomp_bw_put.txt
 srun -n 2 run.sh ./mpi_bw get > mpi_bw_get.txt
 srun -n 2 run.sh ./mpi_bw put > mpi_bw_put.txt
 
 # Make sure you python3 has correctly installed the packages
-# pip install matplotlib numpy pandas
+# In Perlmutter you could use:
+module load python/3.13
 
 python3 plot_benchmark.py --diomp-get-file diomp_bw_get.txt \
                          --diomp-put-file diomp_bw_put.txt \
